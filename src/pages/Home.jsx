3d6b746dfdceeb84d62ec786e89514731ff9110b
WadeeKT <thomassanna1e@gmail.com>
2023-06-28 20:37:16 +0200

    Update Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import '../styles/Home.css'
 
 function Home() {
   const [data, setData] = useState(null);
@@ -17,22 +18,28 @@ function Home() {
     const searchText = e.target.value.toLowerCase();
     const filtered = data.filter(
       (dat) =>
-        dat.chapter_title.toLowerCase().includes(searchText)
+        dat.chapter_title.toLowerCase().includes(searchText) ||
+        (dat.chapter_description && dat.chapter_description.includes(searchText)) ||
+        dat.chapter_number.slice(4, dat.chapter_number.length).includes(searchText)
     );
     setFilteredData(filtered);
   };
 
   return (
     <div>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Serif:ital,wght@0,400;0,700;1,300&family=Rubik:wght@400;500;700&display=swap" rel="stylesheet" /> 
       <input type="text" className="searchbar" onChange={handleSearch} />
       {filteredData && (
-        <div>
+        <div className="chapter-container">
           {filteredData.map((dat) => (
-            <div className="chapter" key={dat.id}>
-              <p className="number">{dat.chapter_number.slice(3, dat.chapter_number.length)}</p>
-              <p className="titre">{dat.chapter_title}</p>
-              <p className="description">{dat.chapter_description}</p>
-            </div>
+            <p href={'https://littlexgarden.com/one-piece/' + dat.id + '/1'} rel='noreferrer' target="_blank" className="chapter" key={dat.id}>
+              <p className="chapter-number">{dat.chapter_number.slice(3, dat.chapter_number.length)}</p>
+              <p className="chapter-titre">{dat.chapter_title}</p>
+              <p className="chapter-description">{dat.chapter_description? dat.chapter_description : "Ce chapitre n'a pas de description."}</p>
+              <img className="chapter-image" src='https://www.slashfilm.com/img/gallery/one-piece-film-red-showcases-luffys-new-transformation-for-the-first-time/l-intro-1667316814.jpg' alt="" />
+            </p>
           ))}
         </div>
       )}
