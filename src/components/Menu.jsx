cdd7944ce0f5ef6a265c0be5f04948c8a1968251
WadeeKT <thomassanna1e@gmail.com>
2023-07-01 13:21:32 +0200

    Updating Menu.jsx

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -49,7 +49,7 @@ const Menu = () => {
           <li className='menu-c-o'>© Eiichiro Oda - One Piece</li>
           <li className='menu-c-w'>Site internet créé par <a className='credit-link' title='https://twitter.com/wadeekt' href='https://twitter.com/wadeekt'>WadeeKT</a> en colaboration avec <a className='credit-link' title='https://littlexgarden.com' href="https://littlexgarden.com">Little Garden</a>.</li>
         </ul>
-      </div>
+      </div> 
     </div>
   );
 };
