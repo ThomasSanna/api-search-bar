3fadf2fdbb9609b06b7ac2bb1aaaf9876ce6d94f
WadeeKT <thomassanna1e@gmail.com>
2023-12-07 19:45:29 +0100

    Adding some images

--- src/pages/RoadToPiece.jsx
+++ src/pages/RoadToPiece.jsx
@@ -1,25 +1,32 @@
 
 import React, { useEffect } from "react";
 import Footer from "../components/Footer";
+import Menu from "../components/Menu";
 
 const RoadToPiece = () => {
     let images = [];
-    const context = require.context('../assets/images', true);
+    const context = require.context('../assets/RoadToPiece', true);
     images = context.keys().map((image) => image.replace('./', ''));
-    // console.log(images);
+    console.log(images);
 
     return (
         <div className="road-to-piece">
+            <Menu />
             <h1>Road to piece</h1>
-            <p>
-                {/* {
-                    // Utilise maintenant la variable images déclarée à l'extérieur de useEffect
-                    images.map((image) => (
-                        <p>{image}</p>
-                    ))
-                } */}
-                Bientot...
-            </p>
+            <div className="road-to-piece-cont">
+                {images.map((image, index) => {
+                    return (
+                        <div key={index} className="road-to-piece-img-cont">
+                            <img
+                                className="road-to-piece-img"
+                                src={require(`../assets/RoadToPiece/${image}`).default}
+                                alt={image}
+                            />
+                        </div>
+                    );
+                })}
+                <img src="17.png" alt="" />
+            </div>
             <Footer />
         </div>
     );
